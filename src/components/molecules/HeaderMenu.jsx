6f56a2c25562811ff2c23deef5ab88de4630c1ae
Christofer Castillo <ccastillo993a@gmail.com>
2021-09-07 01:11:48 -0500

    Creación del header + estilos

---
+++ src/components/molecules/HeaderMenu.jsx
@@ -0,0 +1,75 @@
+
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import IconCategories from '../icons/IconCategories';
+import IconChat from '../icons/IconChat';
+import IconLogo from '../icons/IconLogo';
+import IconNotificationFull from '../icons/IconNotificationFull';
+import IconPlay from '../icons/IconPlay';
+import IconRanking from '../icons/IconRanking';
+import IconVersus from '../icons/IconVersus';
+
+const HeaderMenu = () => {
+	return (
+		<header className="main-header s-main-center">
+			<div className="ed-grid">
+				<div className="s-cross-center">
+					<div className="s-to-left s-cross-center">
+						<div className="menu-container-logo s-cross-center">
+							<i className="container-icon-logo">
+								<IconLogo />
+							</i>
+							<h4 className={`content-color`}>GuessMusic</h4>
+						</div>
+						<div className="menu-container">
+							<div to="/" className="menu-list m-mr-2">
+								<i className="container-icon-menu">
+									<IconPlay />
+								</i>
+								<h4 className={`content-color`}>Jugar</h4>
+							</div>
+							<div to="/" className="menu-list m-mr-2">
+								<i className="container-icon-menu">
+									<IconRanking />
+								</i>
+								<h4 className={`content-color`}>Clasificación</h4>
+							</div>
+							<div to="/" className="menu-list m-mr-2">
+								<i className="container-icon-menu">
+									<IconCategories />
+								</i>
+								<h4 className={`content-color`}>Categorias</h4>
+							</div>
+							<div to="/" className="menu-list">
+								<i className="container-icon-menu">
+									<IconVersus />
+								</i>
+								<h4 className={`content-color`}>Batalla</h4>
+							</div>
+						</div>
+					</div>
+					<div className="s-to-right s-cross-center">
+						<div className="menu-icons">
+							<div to="/" className="menu-item-icon">
+								<i className="container-icon-nav">
+									<IconNotificationFull/>
+								</i>
+							</div>
+							<div to="/" className="menu-item-icon">
+								<i className="container-icon-nav">
+									<IconChat/>
+								</i>
+							</div>
+							<div class="menu-item-profile">
+								<img alt="profile" src="https://th.bing.com/th/id/OIP.5C7gR_T8k_r45PSdZdR7nAHaHa?pid=ImgDet&rs=1" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default HeaderMenu;
+
---
+++ src/components/molecules/HeaderMenuSecondary.jsx
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import IconLeft from '../icons/IconLeft';
+
+
+const HeaderMenuSecondary = () => {
+	return (
+		<header className="secondary-header s-main-center">
+			<div className="ed-grid">
+				<div className="s-cross-center">
+					<div className="s-to-left s-cross-center">
+						<div className="menu-container-arrow s-cross-center">
+							<i className="container-icon-arrowBackMenu">
+								<IconLeft />
+							</i>
+						</div>
+					</div>
+					<div className="s-to-right s-cross-center">
+						<div className="menu-icons">
+							<div class="menu-item-profile">
+								<img alt="profile" src="https://th.bing.com/th/id/OIP.5C7gR_T8k_r45PSdZdR7nAHaHa?pid=ImgDet&rs=1" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default HeaderMenuSecondary;
+
