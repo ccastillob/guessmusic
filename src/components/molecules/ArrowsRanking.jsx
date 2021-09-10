73c487e9afd31cb95ab83f740f04e75a7c465928
Christofer Castillo <ccastillo993a@gmail.com>
2021-09-10 00:22:04 -0500

    Creación de componente ArrowsRanking + estilos

---
+++ src/components/molecules/ArrowsRanking.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import IconLeft from '../icons/IconLeft'
+import IconRight from '../icons/IconRight'
+
+const ArrowsRanking = () => {
+	return (
+		<div className="ed-grid s-grid-2 m-gap-2 container-arrows_ranking">
+			<div className="ranking__content-left s-cross-center">
+				<div className="content-left__item disabled">
+					<i className="container-icon-arrowLeftRanking">
+						<IconLeft />
+					</i>
+				</div>
+			</div>
+			<div className="ranking__content-right s-cross-center">
+				<div className="content-right__item">
+					<i className="container-icon-arrowRightRanking">
+						<IconRight />
+					</i>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ArrowsRanking
