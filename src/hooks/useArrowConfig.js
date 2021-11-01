2bfccf674e466beee700e07a55870ef760177a15
Christofer Castillo <ccastillo993a@gmail.com>
2021-11-01 01:08:20 -0500

    Creación de hooks para las flechas y las categorias de las canciones

---
+++ src/hooks/useArrowConfig.js
@@ -0,0 +1,35 @@
+
+import ArrowNext from '../components/organisms/ArrowNext';
+import ArrowPrev from '../components/organisms/ArrowPrev';
+
+export const useArrowConfig = () => {
+
+	const settings = {
+    infinite: false,
+    speed: 500,
+    lazyLoad: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    nextArrow: <ArrowNext />,
+    prevArrow: <ArrowPrev />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      }
+    ]
+  };
+
+	return [ settings ];
+
+}
